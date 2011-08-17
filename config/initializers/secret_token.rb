e1bf50557c2c93d9ca2f26ad78fc1eee81dbb3b4
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2011-08-17 13:36:19 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LdLolmausRu::Application.config.secret_token = '8b1eaf5ecc787e2d765f8c5be6210f8ba9655a75c25bd8c8c8c765054348ee903131a5a5c6dec7d9868fd7c39a3f5e3b32592e1549cc02f3516ce512310b4e78'

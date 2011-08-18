830e99a8c01e89f2de187a55d9f080144a22bdd5
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2011-08-18 16:58:36 +0400

    Validation!

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,49 +1,55 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
-  setup do
-    @product = products(:one)
-  end
+	setup do
+		@product =   products(:one)
+		@update  = { title:        'Lorem Ipsum',
+		             manufacturer: 'Vasya',
+		             description:  'Wibbles are fun!',
+		             image_url:    'http://vasya.com/picture.png',
+		             price:        19.95 }
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:products)
-  end
+	end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+	test "should get index" do
+		get :index
+		assert_response :success
+		assert_not_nil assigns(:products)
+	end
 
-  test "should create product" do
-    assert_difference('Product.count') do
-      post :create, :product => @product.attributes
-    end
+	test "should get new" do
+		get :new
+		assert_response :success
+	end
 
-    assert_redirected_to product_path(assigns(:product))
-  end
+	test "should create product" do
+		assert_difference('Product.count') do
+			post :create, :product => @update
+		end
 
-  test "should show product" do
-    get :show, :id => @product.to_param
-    assert_response :success
-  end
+		assert_redirected_to product_path(assigns(:product))
+	end
 
-  test "should get edit" do
-    get :edit, :id => @product.to_param
-    assert_response :success
-  end
+	test "should show product" do
+		get :show, :id => @product.to_param
+		assert_response :success
+	end
 
-  test "should update product" do
-    put :update, :id => @product.to_param, :product => @product.attributes
-    assert_redirected_to product_path(assigns(:product))
-  end
+	test "should get edit" do
+		get :edit, :id => @product.to_param
+		assert_response :success
+	end
 
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete :destroy, :id => @product.to_param
-    end
+	test "should update product" do
+		put :update, :id => @product.to_param, :product => @update
+		assert_redirected_to product_path(assigns(:product))
+	end
 
-    assert_redirected_to products_path
-  end
+	test "should destroy product" do
+		assert_difference('Product.count', -1) do
+			delete :destroy, :id => @product.to_param
+		end
+
+		assert_redirected_to products_path
+	end
 end
